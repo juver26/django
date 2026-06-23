1547d3a2b89064ce5429428eeb5c951a3d363e8d
juver26 <juver2609@gmail.com>
2026-06-23 15:04:50 -0500

    Trabajamos en el registro de usuarios

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+usuarios = []
+def register(request):
+    if request.method == 'post':
+        name = request.post.get('name')
+        password = request.post.get('password')
+        for users in usuarios:
+            if users ['name'] == name:
+                print('Este nombre de usuario ya existe.')
+            else:
+                usuarios.append({'name':name,'password':password})
+def login():
+    pass
 # Create your views here.
