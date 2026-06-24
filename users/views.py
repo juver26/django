b8dae37b6bdac10649f84308a8a082a718c82566
juver26 <juver2609@gmail.com>
2026-06-24 14:59:20 -0500

    Creación de la vista del usuario

--- users/views.py
+++ users/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import redirect, render
-usuarios = []
+from users.models import User
+#usuarios = []
 def register(request):
     if request.method == 'post':
         name = request.post.get('name')
         password = request.post.get('password')
-        for users in usuarios:
-            if users ['name'] == name:
-                print('Este nombre de usuario ya existe.')
-            else:
-                usuarios.append({'name':name,'password':password})
+        print(name)
+        print(password)
+        User.objects.create(username=name,nickname=name,password=password)
+        #for users in usuarios:
+            #if users ['name'] == name:
+                #print('Este nombre de usuario ya existe.')
+            #else:
+                #usuarios.append({'name':name,'password':password})
+    return render(request,'users/register.html')
 def login():
     pass
 # Create your views here.
